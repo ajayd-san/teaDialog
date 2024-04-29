a5db8067f0f1798a95ec6ec5518d7a66b50b0857
Ajay <ajayds2001@gmail.com>
2024-04-28 20:38:57 -0400

    add: keys.go

---
+++ keys.go
@@ -0,0 +1,55 @@
+package main
+
+import "github.com/charmbracelet/bubbles/key"
+
+type navigationKeymap struct {
+	Enter key.Binding
+	Back  key.Binding
+	Quit  key.Binding
+	Next  key.Binding
+	Prev  key.Binding
+}
+
+type promptNavKeymap struct {
+	Enter key.Binding
+	Next  key.Binding
+	Prev  key.Binding
+}
+
+var promptKeymap = promptNavKeymap{
+	Enter: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "select"),
+	),
+	Next: key.NewBinding(
+		key.WithKeys("right", "l"),
+		key.WithHelp("->/l", "next option"),
+	),
+	Prev: key.NewBinding(
+		key.WithKeys("left", "h"),
+		key.WithHelp("<-/h", "prev option"),
+	),
+}
+
+var NavKeymap = navigationKeymap{
+	Enter: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "select"),
+	),
+	Back: key.NewBinding(
+		key.WithKeys("esc"),
+		key.WithHelp("esc", "back"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c", "q"),
+		key.WithHelp("ctrl+c/q", "quit"),
+	),
+	Next: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("->/j", "next prompt"),
+	),
+	Prev: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("<-/k", "prev prompt"),
+	),
+}
