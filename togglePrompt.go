fbf05ba3d6c99bf59daf80edbd120560524cc6ab
Ajay <ajayds2001@gmail.com>
2024-05-02 09:11:40 -0400

    add: added toggle Prompt

---
+++ togglePrompt.go
@@ -0,0 +1,61 @@
+package teadialog
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type TogglePrompt struct {
+	message  string
+	selected bool
+	isActive bool
+}
+
+func (m TogglePrompt) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, promptKeymap.Select):
+			m.selected = !m.selected
+		}
+
+	}
+	return m, nil
+}
+
+func (m TogglePrompt) View() string {
+	var res strings.Builder
+	var checkbox string
+	checkbox = checkBox
+
+	if m.selected {
+		checkbox = checkMark
+	}
+
+	str := fmt.Sprintf("%s %s", checkbox, m.message)
+	if m.isActive {
+		res.WriteString(selectedPromptOptionStyle.Render(str))
+	}
+	res.WriteString("\n\n")
+
+	return promptStyle.Render(res.String())
+}
+
+func (m TogglePrompt) Init() tea.Cmd {
+	return nil
+}
+
+//util
+
+func (p *TogglePrompt) setIsActive(state bool) {
+	p.isActive = state
+}
+
+func MakeTogglePrompt(message string) TogglePrompt {
+	return TogglePrompt{
+		message: message,
+	}
+}
