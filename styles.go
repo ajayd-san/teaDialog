8a26081451df6042be6f1795db815bd39dc0ac7c
Ajay <ajayds2001@gmail.com>
2024-05-17 19:39:32 -0400

    mod: prompt styling

--- styles.go
+++ styles.go
@@ -6,8 +6,8 @@ var (
 	containerStyle            = lipgloss.NewStyle().Align(lipgloss.Center).AlignVertical(lipgloss.Center)
 	dialogStyle               = lipgloss.NewStyle().Border(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("69")).Align(lipgloss.Center).Padding(2, 8)
 	promptContainerStyle      = lipgloss.NewStyle().Align(lipgloss.Left)
-	promptStyle               = lipgloss.NewStyle()
-	selectedPromptStyle       = lipgloss.NewStyle().Border(lipgloss.NormalBorder()).BorderRight(false).BorderTop(false).BorderBottom(false)
+	promptStyle               = lipgloss.NewStyle().MarginLeft(1)
+	selectedPromptStyle       = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), false, false, false, true).Padding(0, 0, 0, 1)
 	selectedPromptOptionStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("98"))
 )
 
